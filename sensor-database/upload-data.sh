5bf8f681a92ccfad18c2f0d2a9acae8a19a05504
Mercedes Miñano Moya <mercedes.minano@cern.ch>
2015-06-10 17:58:09 +0200

    Update upload-data.sh

--- sensor-database/upload-data.sh
+++ sensor-database/upload-data.sh
@@ -3,14 +3,15 @@
 BATCH=$1
 WAFER=$2
 SENSOR=$3
+STEP=$4
 sensorID=S${1}-${2}-${3}
 
 if [ ${1} == '' ]; then
-    echo "Please give me the sensorID like '322497 XX X'"
+    echo "Please give me the sensorID like '322497 XX X STEP'"
     exit 1   
 fi
 
-cd SensorTest_${sensorID}_BAM
+cd SensorTest_${sensorID}_${STEP}
 
-TARFILE="SensorTest_${sensorID}_BAM.tar.gz"
+TARFILE="SensorTest_${sensorID}_${STEP}.tar.gz"
 scp -P 23481 ${TARFILE} cern@cmspixelprod.pi.infn.it:/home/cern/dropbox
